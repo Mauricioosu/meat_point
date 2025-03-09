b76f31726842fc3b9465ea5c6214ea754e2817b9
Mauricioosu <mauricioosuna3@gmail.com>
2025-03-09 19:59:40 -0400

    Att

---
+++ start.py
@@ -0,0 +1,14 @@
+temperatura = int(input('Qual temperatura esta a carne? '))
+
+if temperatura >= 71:
+    print('A carne esta bem passada')
+elif temperatura >= 65:
+    print('A carne esta ao ponto para bem passado')
+elif temperatura >= 60:
+    print('A carne esta ao ponto')
+elif temperatura >= 54:
+    print('A carne esta ao ponto para mal passado')
+elif temperatura >= 48:
+    print('A carne esta selada')
+else:
+    print('A carne esta Crua')
