d4acd8bd488a1f834b0f09f44f380fdb2e1d06ec
Mauricioosu <mauricioosuna3@gmail.com>
2025-03-23 22:52:49 -0400

    iniciado Front ultilizando Flask

--- Main.py
+++ Main.py
@@ -1,7 +1,3 @@
-"""
-Objetivo: Receber a temperatura da carne e informar o ponto da carne conforme a temperatura.
-"""
-
 def definir_ponto_carne(temperatura: float) -> str:
     """Retorna o ponto da carne com base na temperatura informada."""
     if temperatura >= 71:
@@ -17,19 +13,19 @@ def definir_ponto_carne(temperatura: float) -> str:
     else:
         return "Crua"
 
+
 def main():
     """Função principal para entrada e saída de dados."""
     try:
-        temperatura = float(input("Qual a temperatura da carne (°C)? ").strip())
-        
+        temperatura = float(input("Qual a temperatura da carne (°C)? ").strip())        
         if temperatura < 0:
             print("A temperatura não pode ser negativa. Insira um valor válido.")
             return
-        
         ponto = definir_ponto_carne(temperatura)
         print(f"A carne está: {ponto}")
     except ValueError:
         print("Por favor, insira um valor numérico válido.")
+
 
 if __name__ == "__main__":
     main()
